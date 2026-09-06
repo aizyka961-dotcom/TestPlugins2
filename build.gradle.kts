49b1128835f7c97a16bd24147932eb52db83ca29
aizyka961-dotcom <aizyka961@gmail.com>
2026-09-06 15:38:52 +0700

    Update build.gradle.ktsj

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
         // Cloudstream gradle plugin which makes everything work and builds plugins
-        classpath("com.github.recloudstream:gradle:32895aedb6366f5075cb99bbd2e6ce0a7cac325d")
+        classpath("com.github.recloudstream:gradle:master")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
